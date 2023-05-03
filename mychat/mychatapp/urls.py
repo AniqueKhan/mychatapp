070f0ea8d6b4896100be1ac32fb8fcc0dfe27bce
Anique <aniquekhan004@gmail.com>
2023-05-03 23:08:40 +0500

    done till 38:09 part two
    done till 38:09 part two

---
+++ mychat/mychatapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import index,detail
+urlpatterns = [
+    path("",index,name="index"),
+    path("friends/<int:pk>",detail,name="detail")
+]
--- mychat/mychatapp/views.py
+++ mychat/mychatapp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Friend,Profile,ChatMessage
+from .forms import ChatMessageForm
 # Create your views here.
+
+def index(request):
+    user = request.user.profile
+    friends = user.friends.all()
+    context = {
+        "user":user,
+        "friends":friends,
+    }
+    return render(request, "index.html",context)
+
+def detail(request,pk):
+    friend = Friend.objects.get(profile_id=pk)
+    chats = ChatMessage.objects.all()
+    user = request.user.profile # msg_sender
+    profile = Profile.objects.get(id=friend.profile.id) # msg_reciever
+    form = ChatMessageForm()
+
+    if request.method == "POST":
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            chat_msg = form.save(commit=False)
+            chat_msg.msg_sender = user
+            chat_msg.msg_reciever = profile
+            chat_msg.save()
+            return redirect("detail",pk=friend.profile.id)
+
+
+    context = {
+        "friend":friend,
+        "form":form,
+        "profile":profile,
+        "user":user,
+        "chats":chats,
+    }
+    return render(request, "detail.html",context)
