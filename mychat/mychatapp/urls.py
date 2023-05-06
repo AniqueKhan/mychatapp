9f6c52345543ae416ef1d49db2b8e737a3389d42
Anique <aniquekhan004@gmail.com>
2023-05-06 17:04:31 +0500

    tutorial done , do testing
    tutorial done , do testing

--- mychat/mychatapp/urls.py
+++ mychat/mychatapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index,detail
+from .views import index,detail,sendMessage,receivedMessages,chatNotification
 urlpatterns = [
     path("",index,name="index"),
-    path("friends/<int:pk>",detail,name="detail")
+    path("friends/<int:pk>",detail,name="detail"),
+    path("send_msg/<int:pk>",sendMessage,name="send_msg"),
+    path("receive_msgs/<int:pk>",receivedMessages,name="receive_msgs"),
+    path("chat_notification",chatNotification,name="chat_notification")
 ]
--- mychat/mychatapp/views.py
+++ mychat/mychatapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Friend,Profile,ChatMessage
 from .forms import ChatMessageForm
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def index(request):
@@ -17,6 +19,9 @@ def detail(request,pk):
     chats = ChatMessage.objects.all()
     user = request.user.profile # msg_sender
     profile = Profile.objects.get(id=friend.profile.id) # msg_reciever
+    recieved_chats = ChatMessage.objects.filter(msg_sender=profile,msg_reciever=user)
+    recieved_chats.update(seen=True)
+
     form = ChatMessageForm()
 
     if request.method == "POST":
@@ -35,5 +40,32 @@ def detail(request,pk):
         "profile":profile,
         "user":user,
         "chats":chats,
+        "recieved_chats":recieved_chats,
     }
     return render(request, "detail.html",context)
+
+
+def sendMessage(request,pk):
+    user = request.user.profile
+    friend = Friend.objects.get(profile_id=pk)
+    profile = Profile.objects.get(id=friend.profile.id)
+    msg = json.loads(request.body)['msg']
+    chat_msg = ChatMessage.objects.create(body=msg,msg_reciever=profile,msg_sender=user)
+    print(msg)
+    return JsonResponse(chat_msg.body,safe=False)
+
+def receivedMessages(request,pk):
+    user = request.user.profile
+    friend = Friend.objects.get(profile_id=pk)
+    profile = Profile.objects.get(id=friend.profile.id)
+    chats = ChatMessage.objects.filter(msg_sender=profile,msg_reciever=user)
+    chats_msgs = [chat.body for chat in chats]
+    return JsonResponse(chats_msgs,safe=False)
+
+def chatNotification(request):
+    user = request.user.profile
+    friends = user.friends.all()
+    chats_counts=[
+        ChatMessage.objects.filter(msg_sender__id=friend.profile.id,msg_reciever=user,seen=False).count() for friend in friends
+    ]
+    return JsonResponse(chats_counts,safe=False)
